0a887cbfa9e3247c57e45117bb1ed907b4d791f4
tilleryd <tilleryd@gmail.com>
2015-05-14 10:16:24 -0400

    Handle errors on watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,42 @@
-var gulp = require('gulp');
 var babel = require('gulp-babel');
-var browserify = require('browserify');
 var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var gulp = require('gulp');
 var less = require('gulp-less');
- 
-gulp.task('build', function() {
-  browserify({
+var source = require('vinyl-source-stream');
+
+function onError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
+
+gulp.task('javascript', function(){
+  var b = browserify({
     entries: 'app.js',
     paths: './src',
-    debug: false
-  })
-  .transform(babelify)
-  .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('./dist'));
+    debug: false,
+    transform: [babelify]
+  });
+  
+  return b.bundle()
+    .on('error', onError)
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist'));
+});
 
+gulp.task('less', function() {
   gulp
     .src('./less/**/*.less')
     .pipe(less())
+    .on('error', onError)
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./src/**/*.js'], ['build']);
-  gulp.watch(['./less/**/*.less'], ['build']);
+  gulp.watch(['./src/**/*.js'], ['javascript']);
+  gulp.watch(['./less/**/*.less'], ['less']);
 });
  
-gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['javascript', 'less']);
+
+gulp.task('default', ['build']);
